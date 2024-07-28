77b63811d7b56f6b4a8ce082bddf8fc986bd887a
oleksandrkovaliuk <sasha1235qaz@ukr.net>
2024-07-28 12:14:08 -0600

    added:(api functions to managing listings page)

---
+++ src/api/listings/getTypeOfPlace.js
@@ -0,0 +1,15 @@
+const db = require("../../database");
+const getTypeOfPlaceQuery = "SELECT * FROM type_of_place;";
+
+const getTypeOfPlace = async (req, res) => {
+  try {
+    await db.query(getTypeOfPlaceQuery, (dbError, dbResponse) => {
+      if (dbError) res.status(500).json({ message: "Internal Server Error" });
+      return res.status(200).json({ data: dbResponse?.rows });
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = getTypeOfPlace;
