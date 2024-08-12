c34c5816cdf013d2f36f03b26abb65d41f4f0512
oleksandrkovaliuk <sasha1235qaz@ukr.net>
2024-08-12 16:50:55 -0600

    implemented(:logic of creating listing)

---
+++ src/api/listings/getListings.js
@@ -0,0 +1,14 @@
+const db = require("../../database");
+
+const getListings = async (req, res) => {
+  try {
+    const dbResponse = await db.query("SELECT * FROM listings");
+    return res.status(200).json({ data: dbResponse?.rows });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error. Please try again" });
+  }
+};
+
+module.exports = getListings;
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -7,6 +7,8 @@ const checkAuthType = require("../api/authorization/checkAuthType");
 
 const getListingCategories = require("../api/listings/getListingsCategories");
 const getTypeOfPlace = require("../api/listings/getTypeOfPlace");
+const createListing = require("../api/listings/createListing");
+const getListings = require("../api/listings/getListings");
 
 // AUTH
 router.route("/auth/accessUser").post(accessUser);
@@ -14,6 +16,8 @@ router.route("/auth/oauthUser").post(insertOAuthUser);
 router.route("/auth/checkAuthType").post(checkAuthType);
 
 // LISTINGS
+router.route("/listings/listings").get(getListings);
 router.route("/listings/categories").get(getListingCategories);
 router.route("/listings/typeofplace").get(getTypeOfPlace);
+router.route("/listings/createListing").post(createListing);
 module.exports = router;
